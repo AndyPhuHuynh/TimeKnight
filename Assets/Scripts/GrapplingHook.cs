39f159aff59b3a0dc80affff5236d68ce992ba33
AndyPhuHuynh <andyphuhuynh4@gmail.com>
2026-03-11 09:57:23 -0400

    Fixed naming conventions

--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -1,18 +1,3 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class GrapplingHook : MonoBehaviour
-{
-    private Vector2 _mousePosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        var mouse = Mouse.current.position;
-    }
-}
+public class GrapplingHook : MonoBehaviour { }
