7954df911045098b941cc4f3b1321d3ba6d0ce3c
AndyPhuHuynh <andyphuhuynh4@gmail.com>
2026-03-13 10:05:10 -0400

    Add object pooling for the buttons to reduce lag

--- Assets/Scripts/TimeKnight/Interaction/SelectorUI.cs
+++ Assets/Scripts/TimeKnight/Interaction/SelectorUI.cs
@@ -8,14 +8,59 @@ namespace TimeKnight.Interaction
     public class SelectorUI : MonoBehaviour
     {
         [SerializeField] private Button selectorButtonPrefab;
-        [SerializeField] private GameObject selectorButtonPanel;
-
+        [SerializeField] private GameObject selectorButtonContainer;
+        [SerializeField] private GameObject selectorButtonPoolContainer;
+        
+        private readonly Queue<Button> _buttonPool = new();
         private readonly Dictionary<IInteractable, Button> _buttonMap = new();
+
+        private void Awake()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var button = InstantiateButton();
+                ReturnButton(button);
+            }
+        }
+
+        private Button InstantiateButton() => Instantiate(selectorButtonPrefab);
+
+        private static void HideButton(Button button)
+        {
+            var cg = button.GetComponentInChildren<CanvasGroup>();
+            cg.alpha = 0;
+            cg.interactable = false;
+            cg.blocksRaycasts = false;
+        }
+
+        private static void ShowButton(Button button)
+        {
+            var cg = button.GetComponentInChildren<CanvasGroup>();
+            cg.alpha = 1;
+            cg.interactable = true;
+            cg.blocksRaycasts = true;
+        }
+        
+        private Button GetButton()
+        {
+            return _buttonPool.Count > 0 ? _buttonPool.Dequeue() : InstantiateButton();
+        }
+
+        private void ReturnButton(Button button)
+        {
+            HideButton(button);
+            button.transform.SetParent(selectorButtonPoolContainer.transform);
+            button.transform.SetAsFirstSibling();
+            _buttonPool.Enqueue(button);
+        }
         
         public void AddInteractable(IInteractable interactable)
         {
-            var button = Instantiate(selectorButtonPrefab, selectorButtonPanel.transform);
+            var button = GetButton();
+            button.transform.SetParent(selectorButtonContainer.transform);
+            button.transform.SetAsLastSibling();
             button.GetComponentInChildren<TextMeshProUGUI>().text = interactable.InteractionName;
+            ShowButton(button);
             _buttonMap[interactable] = button;
             gameObject.SetActive(true);
         }
@@ -23,7 +68,7 @@ namespace TimeKnight.Interaction
         public void RemoveInteractable(IInteractable interactable)
         {
             if (!_buttonMap.Remove(interactable, out var button)) return;
-            Destroy(button.gameObject);
+            ReturnButton(button);
             if (_buttonMap.Count == 0)
             {
                 gameObject.SetActive(false);
